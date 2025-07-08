4c17320d4368c0160b812a3dea613244c54f39b1
Arvin.Chan <arvin.chen@zkteco.com>
2025-07-08 10:34:59 +0800

    add footer

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,37 @@
+import {Box, Divider, Link, Stack, Typography} from "@mui/material";
+import {useTranslation} from "react-i18next";
+
+
+const Footer = () => {
+  const {t} = useTranslation()
+  return (
+    <Box
+      component={'footer'}
+      sx={{
+        height: '128px',
+        width: "100%",
+        backgroundColor: '#fff',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        bottom: 0
+      }}
+    >
+      <Stack spacing={2} flex={1} sx={{pt: 2}}>
+        <Box>
+          <Typography>Calculators</Typography>
+          <Stack direction={'row'} spacing={1}>
+            <Link href={'/bmi-calculator'}>{t('bmi.calculator')}</Link>
+            <Link href={'/loan-payoff-calculator'}>{t('payment.calculator')}</Link>
+            <Link href={'/tip-calculator'}>{t('tip.calculator')}</Link>
+            <Link href={'/percent-off-calculator'}>{t('percentOff.calculator')}</Link>
+            <Link href={'/circumference-calculator'}>{t('circumference.calculator')}</Link>
+          </Stack>
+        </Box>
+      </Stack>
+      <Typography sx={{padding: "8px 0"}}>© {new Date().getFullYear()} calculator-now.com. All rights reserved.</Typography>
+    </Box>
+  )
+}
+export default Footer
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Grid from "@mui/material/Grid2";
 import {Outlet} from "react-router-dom";
+import Footer from "@/layout/Footer";
 
 
 const MainLayout = () => {
@@ -9,16 +10,27 @@ const MainLayout = () => {
       container
       sx={{
         height: "100%",
+        display: "flex",
+        flexDirection: "column"
       }}
     >
-      <Grid size={12}>
+      <Grid size={12} sx={{height: "64px"}}>
         <Header/>
       </Grid>
-      <Grid size={2}/>
-      <Grid size={8} sx={{p: 2}}>
-        <Outlet/>
+      <Grid size={12} container sx={{height: 'calc(100%-64px-128px)'}}>
+        <Grid size={2}/>
+        <Grid
+          size={8}
+          sx={{
+            p: 2,
+          }}>
+          <Outlet/>
+        </Grid>
+        <Grid size={2}/>
       </Grid>
-      <Grid size={2}/>
+      <Grid size={12}>
+        <Footer/>
+      </Grid>
     </Grid>
   )
 }
