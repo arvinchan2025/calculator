508b0e064e9659305c476a02f55223c529c69564
Arvin.Chan <arvinchan2025@gmail.com>
2025-08-01 22:59:15 +0800

    add vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
   return (
     <BrowserRouter basename={'/'}>
       <Routes>
-        <Route path={'/es/*'} element={<MainRouter/>}/>
-        <Route path={'/ar/*'} element={<MainRouter/>}/>
+        <Route path={'es/*'} element={<MainRouter/>}/>
+        <Route path={'ar/*'} element={<MainRouter/>}/>
         {/*<Route path={'/zh/*'} element={<MainRouter/>}/>*/}
-        <Route path={'/*'} element={<MainRouter/>}/>
+        <Route path={'*'} element={<MainRouter/>}/>
       </Routes>
     </BrowserRouter>
   );
